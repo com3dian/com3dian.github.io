6d5a976224f037ea079eba036f562a084d407a12
Zehao Lu <com3dian@outlook.com>
2026-05-27 21:40:43 +0200

    update may27

--- app/layout.tsx
+++ app/layout.tsx
@@ -94,7 +94,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <meta name="theme-color" media="(prefers-color-scheme: light)" content="#fff" />
       <meta name="theme-color" media="(prefers-color-scheme: dark)" content="#000" />
       <link rel="alternate" type="application/rss+xml" href={`${basePath}/feed.xml`} />
-      <body className="bg-white pl-[calc(100vw-100%)] text-gray-900 antialiased dark:bg-[rgb(57, 57, 57)] dark:text-[rgb(222,222,222)]">
+      <body className="dark:bg-[rgb(57, 57, 57)] bg-white pl-[calc(100vw-100%)] text-gray-900 antialiased dark:text-[rgb(222,222,222)]">
         <ThemeProviders>
           <Analytics analyticsConfig={siteMetadata.analytics as AnalyticsConfig} />
           <SectionContainer>
