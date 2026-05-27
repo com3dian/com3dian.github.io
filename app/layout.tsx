0379eeb71679b6ad3e1b4a3fc493a6b71c287f3e
Zehao Lu <com3dian@outlook.com>
2026-05-27 23:16:35 +0200

    may 27th

--- app/layout.tsx
+++ app/layout.tsx
@@ -68,21 +68,14 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       suppressHydrationWarning
     >
       <link
+        rel="icon"
+        type="image/svg+xml"
+        href={`${basePath}/static/favicons/favicon.svg`}
+      />
+      <link
         rel="apple-touch-icon"
         sizes="76x76"
         href={`${basePath}/static/favicons/apple-touch-icon.png`}
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="32x32"
-        href={`${basePath}/static/favicons/favicon-32x32.png`}
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="16x16"
-        href={`${basePath}/static/favicons/favicon-16x16.png`}
       />
       <link rel="manifest" href={`${basePath}/static/favicons/site.webmanifest`} />
       <link
